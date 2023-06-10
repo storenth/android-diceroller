cbe32e5824a9379f3f11fde0579636410505e97e
Kirill Zhdanov <storenth@yandex.ru>
2023-06-10 23:48:50 +0700

    feat: add uiautomator deps and simple test
    introducing `UI Automator`

--- app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
@@ -1,7 +1,11 @@
 package com.example.diceroller
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiObject2
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +19,20 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private val tag = "androidTest"
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.diceroller", appContext.packageName)
+    }
+    @Test
+    fun textOnBackground() {
+        // Text rendered after app is booted.
+        var device: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        Log.v(tag, device.currentPackageName)
+        device.pressHome()
+        Log.v(tag, device.currentPackageName)
+        println(device.currentPackageName)
     }
 }
